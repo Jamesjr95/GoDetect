08bc975de06077cc40c6c7936c69601589eda114
James Johnson <jamesjhnsn1995@gmail.com>
2023-06-25 03:43:30 -0700

    provisioned resources through iac

---
+++ src/main.tf
@@ -0,0 +1,19 @@
+terraform {
+    required_version = ">=0.13.0"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~>3.0"
+        }
+    }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "tf-state" {
+  source = "./modules/tf-state"
+  bucket_name = "ga-tf-state-backend-ci-cd"
+}
+
